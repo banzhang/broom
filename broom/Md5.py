b921b04879279edb5817a8b30b36bbc491ca4b85
root <root@localhost.localdomain>
2016-03-07 22:33:15 +0800

    控制速度为3m/s

--- broom/Md5.py
+++ broom/Md5.py
@@ -22,9 +22,9 @@ def GetFileMd5(filename):
     i = 0
     while True:
         i = i+1
-        b = f.read(1024)
-        size = i*1024
-        if i>0 and (size%2097152) == 0:
+        b = f.read(6144)
+        size = i*6144
+        if i>0 and (size%3145728) == 0:
             logger.info(filename+',read:'+str(size/1048576)+'Mbytes')
             time.sleep(1)
         
